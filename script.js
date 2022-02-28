96a1405bddc977947fc0223771a39496d3db2c2d
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-02-28 15:56:23 -0500

    Add style dropdown of mobile toggle

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 const toggleMenu = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.desktoppp');
 const closeMenu = document.querySelector('.bars');
+const listMenu = document.querySelector('.menu')
 
 
 toggleMenu.addEventListener('click', function () {
     console.log("Test");
     mobileMenu.classList.toggle('visible');
     closeMenu.classList.toggle('visible');
+    listMenu.classList.toggle('visible');
 })
