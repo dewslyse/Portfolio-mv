7828388c3df17949731ade603785909730e4121a
dewslyse <dewslyse@gmail.com>
2022-03-01 04:32:31 +0000

    Resolve linter errors

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-//Toggle mobile menu
+// Toggle mobile menu
 const openMenu = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.desktop');
 const menuItems = document.querySelectorAll('.menu-item');
 
 openMenu.addEventListener('click', () => {
-    openMenu.classList.toggle('hide');
+  openMenu.classList.toggle('hide');
+  mobileMenu.classList.toggle('show');
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener('click', () => {
     mobileMenu.classList.toggle('show');
-})
-
-menuItems.forEach(item => {
-    item.addEventListener('click', () => {
-        mobileMenu.classList.toggle('show');
-        openMenu.classList.toggle('hide');
-    })
-})
-
+    openMenu.classList.toggle('hide');
+  });
+});
